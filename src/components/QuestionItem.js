7c09ebe6ed32c8b40c31e02d511f1028aa5cc1b7
Abogeerick <abogeerick@gmail.com>
2023-07-25 12:14:55 +0300

    Complete assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDeleteClick() {
+    onDeleteQuestion(id); // Call the onDeleteQuestion prop function with the question id
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -17,7 +21,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionForm from "./QuestionForm";
 
 function QuestionList() {
+  const [questions, setQuestionsItem]=useState([])
+
+  useEffect(()=>{
+    fetch(" http://localhost:4000/questions")
+    .then((res)=>res.json())
+    .then((questions)=>setQuestionsItem(questions))
+  }, [])
+
+  function handleAddQuestion(newQuestion){
+    setQuestionsItem([...questions, newQuestion])
+  }
   return (
     <section>
+    <QuestionForm onAddQuestion={handleAddQuestion} />
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+         <li key={question.id}>{question.prompt}</li>
+        ))}
+      </ul>
     </section>
   );
 }
